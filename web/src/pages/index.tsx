f185e41c8397e5e23076593efc92e4128d665077
ronniekram <arynn.boniface@gmail.com>
2023-06-07 22:41:50 -0400

    fix: footer, index sizing

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -9,9 +9,9 @@ import { DevBurst, CityBurst } from "@/assets/burst";
 
 //! ----------> STYLES <----------
 const Wrapper = styled.section`
-  ${tw`mx-auto w-[fit-content] h-[fit-content] max-w-[76.75rem] xl:(w-[78%])`};
+  ${tw`mx-auto w-[fit-content] h-[fit-content] max-w-[72.75rem] xl:(w-[78%])`};
   ${tw`flex flex-col justify-center`};
-  ${tw`py-32 md:(py-20)`};
+  ${tw`py-32 md:(py-20) xl:(py-12)`};
 `;
 
 //! ----------> COMPONENTS <----------
